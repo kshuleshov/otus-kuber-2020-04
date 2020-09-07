0fe17345703b96e0a532f64452e9eea5509114aa
Konstantin Shuleshov <Konstantin.Shuleshov@nexign.com>
2020-09-07 18:46:55 +0300

    fix ingress controller

--- gopath/src/ingress-host-manager/controllers/ingress_controller.go
+++ gopath/src/ingress-host-manager/controllers/ingress_controller.go
@@ -19,25 +19,21 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"reflect"
-	"strings"
+	"regexp"
 
 	"github.com/go-logr/logr"
 	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-const (
-	hostIpPattern          = ".0.0.0.0."
-	ingressClassAnnotation = "kubernetes.io/ingress.class"
-	ingressClassSuffix     = "-ihm"
+var (
+	hostIpFormat  = ".%s."
+	hostIpPattern = regexp.MustCompile(
+		`\.(([0–9]|[1–9][0–9]|1[0–9]{2}|2[0–4][0–9]|25[0–5])\.){3}([0–9]|[1–9][0–9]|1[0–9]{2}|2[0–4][0–9]|25[0–5])\.`)
 )
 
 // IngressReconciler reconciles a Ingress object
@@ -68,53 +64,18 @@ func (r *IngressReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return reconcile.Result{}, err
 	}
 
-	ingressClass := instance.Annotations[ingressClassAnnotation]
-	if strings.HasSuffix(ingressClass, ingressClassSuffix) {
-		ingress := newIngress(instance)
-
-		found := &extensionsv1beta1.Ingress{}
-		err := r.Client.Get(ctx, types.NamespacedName{Namespace: ingress.Namespace, Name: ingress.Name}, found)
-		if err != nil {
-			if errors.IsNotFound(err) {
-				if err := controllerutil.SetControllerReference(instance, ingress, r.Scheme); err != nil {
-					return reconcile.Result{}, err
-				}
-				logger.Info("Creating a new Ingress",
-					"Ingress.Namespace", ingress.Namespace, "Ingress.Name", ingress.Name,
-					"Ingress.Annotations", ingress.Annotations, "Ingress.Spec", ingress.Spec)
-				return reconcile.Result{}, r.Client.Create(ctx, ingress)
-			}
-			// Error reading the object - requeue the request.
-			return reconcile.Result{}, err
-		}
-
-		if reflect.DeepEqual(ingress.Annotations, found.Annotations) &&
-			reflect.DeepEqual(ingress.Spec, found.Spec) {
-
-			return reconcile.Result{}, nil
-		}
-
-		found.Annotations = ingress.Annotations
-		found.Spec = ingress.Spec
-
-		logger.Info("Updating an Ingress",
-			"Ingress.Namespace", found.Namespace, "Ingress.Name", found.Name,
-			"Ingress.Annotations", found.Annotations, "Ingress.Spec", found.Spec)
-		return reconcile.Result{}, r.Client.Update(ctx, found)
-	}
-
 	for _, igr := range instance.Status.LoadBalancer.Ingress {
 		if igr.IP != "" {
 			update := false
 			for i, rule := range instance.Spec.Rules {
-				if host, found := replaceIp(rule.Host, igr.IP); found {
+				if host, changed := replaceIp(rule.Host, igr.IP); changed {
 					instance.Spec.Rules[i].Host = host
 					update = true
 				}
 			}
 			for i, tls := range instance.Spec.TLS {
 				for j, host := range tls.Hosts {
-					if host, found := replaceIp(host, igr.IP); found {
+					if host, changed := replaceIp(host, igr.IP); changed {
 						instance.Spec.TLS[i].Hosts[j] = host
 						update = true
 					}
@@ -139,33 +100,7 @@ func (r *IngressReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-func newIngress(src *extensionsv1beta1.Ingress) *extensionsv1beta1.Ingress {
-	annotations := make(map[string]string)
-	if src.Annotations != nil {
-		for key, val := range src.Annotations {
-			switch key {
-			case ingressClassAnnotation:
-				annotations[ingressClassAnnotation] = strings.TrimSuffix(val, ingressClassSuffix)
-			default:
-				annotations[key] = val
-			}
-		}
-	}
-	ingress := &extensionsv1beta1.Ingress{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:        src.Name + ingressClassSuffix,
-			Namespace:   src.Namespace,
-			Annotations: annotations,
-		},
-	}
-	src.Spec.DeepCopyInto(&ingress.Spec)
-
-	return ingress
-}
-
 func replaceIp(host, ip string) (string, bool) {
-	if strings.Contains(host, hostIpPattern) {
-		return strings.ReplaceAll(host, hostIpPattern, fmt.Sprintf(".%s.", ip)), true
-	}
-	return host, false
+	replace := hostIpPattern.ReplaceAllLiteralString(host, fmt.Sprintf(hostIpFormat, ip))
+	return replace, host != replace
 }
